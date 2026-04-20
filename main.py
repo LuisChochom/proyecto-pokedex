b0a016067f1b4b500208117b6f4a4afd59267e95
luischochom <luischochom2015@gmail.com>
2026-04-20 09:49:33 -0600

    Codigo nuevo en main

--- main.py
+++ main.py
@@ -32,4 +32,22 @@ def ejecutar_simulacion():
             p2 = crear_objeto_pokemon(p2_idx)
             print(f"Jugador 2 ha elegido a {p2.nombre}.")
         
-        
+        while p1.hp_actual > 0 and p2.hp_actual > 0:
+            print(f"\nTurno de {p1.nombre} (HP: {p1.hp_actual}/{p1.hp_maximo}, Energía: {p1.energia_actual}/{p1.energia_maxima})")
+            procesar_turno(p1, p2)
+            if p2.hp_actual <= 0: break
+
+            print(f"\nTurno de {p2.nombre} (HP: {p2.hp_actual}/{p2.hp_maximo}, Energía: {p2.energia_actual}/{p2.energia_maxima})")
+            if modo == "2":
+                accion_ia = random.choice("1", "2", "3")
+                ejecutar_accion(p2, p1, accion_ia)
+            else:
+                procesar_turno(p2, p1)
+            
+        ganador = p1.nombre if p1.hp_actual > 0 else p2.nombre
+        print(f"\n¡{ganador} ha ganado la batalla!")
+    except (ValueError, KeyError):
+        print("\n[ERROR] Entrada no válida. Use solo números del catálogo.")
+        ejecutar_simulacion()
+    
+    
